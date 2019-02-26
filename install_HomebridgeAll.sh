7e00b3660810e5fd58d6e7a1ce2f761d36d89faa
Rabelbeat <20260158+Rabelbeat@users.noreply.github.com>
2019-02-26 17:24:44 +0200

    Update install_HomebridgeAll.sh

--- install_HomebridgeAll.sh
+++ install_HomebridgeAll.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+CPU="`uname -m`"
+install_mosquitto=0
+install_tasmota=0
+install_shairport=0
+
 print_status() {
     echo
-    echo "## $1"
+    echo ">> $1"
     echo
 }
 
@@ -29,124 +35,260 @@ print_bold() {
     text="$2"
 
     echo
-    echo "${blue}================================================================================${normal}"
-    echo "${blue}================================================================================${normal}"
+    echo "${blue}============================================================================================${normal}"
+    echo "${blue}============================================================================================${normal}"
     echo
-    echo -e "${bold}${green}${title}${normal}"
+    echo "${bold}${green}${title}${normal}"
     echo
-    echo "${blue}================================================================================${normal}"
-    echo "${blue}================================================================================${normal}"
+    echo "${blue}============================================================================================${normal}"
+    echo "${blue}============================================================================================${normal}"
 }
 
 bail() {
-    echo 'Error executing command, exiting'
+    echo 'Fail execut command!, exiting installation'
     exit 1
 }
 
-exec_cmd_nobail() {
+cmd_run_nobail() {
     echo ">> $1"
     bash -c "$1"
 }
 
-exec_cmd() {
-    exec_cmd_nobail "$1" || bail
+cmd_run() {
+    cmd_run_nobail "$1" || bail
 }
-print_bold "Hello,  This script will install Homebridge Server in your Pi\n\n Press Enter to contine or CTRL + C to abort"
-while true 
+
+post_installation(){
+echo "Post installation"
+sudo systemctl stop homebridge.service
+cmd_run 'sudo rm -rf /etc/systemd/system/homebridge.service';
+cmd_run 'sudo systemctl daemon-reload';
+cmd_run 'sudo rm -rf HB_Installation';
+sudo userdel homebridge
+cmd_run 'sudo rm -rf /var/homebridge'
+cmd_run 'sudo mkdir /var/homebridge'
+}
+armv6_installation(){
+echo "You appear to be running on $CPU hardware."
+	cmd_run 'sudo apt-get update';
+	cmd_run 'sudo apt-get install -y git make curl';
+	print_status "##### Installing Node.Js V8.9.4 For Raspberry Pi A/B/B+/Zero ######\n\n";
+	cmd_run 'sudo rm -rf node-v8.9.4-linux-armv6l/'
+	cmd_run 'wget https://nodejs.org/dist/v8.9.4/node-v8.9.4-linux-armv6l.tar.gz';
+	cmd_run 'tar -xvf node-v8.9.4-linux-armv6l.tar.gz';
+	cmd_run 'sudo cp -r node-v8.9.4-linux-armv6l/* /usr/local/';
+	sudo npm uninstall -g homebridge
+	sudo npm uninstall -g homebridge-config-ui-x
+	sudo npm uninstall -g homebridge--gpio-device
+	sudo npm uninstall -g homebridge-mqtt-switch-tasmota
+	print_status "##### Downloading Homebridge Config files & create systemd fail-over service ######\n\n";
+	cmd_run 'git clone https://github.com/Rabelbeat/HB_Installation.git';
+	cmd_run 'sudo cp HB_Installation/service_environment /etc/default/homebridge';
+	cmd_run 'sudo cp HB_Installation/rpi_2-3_Service /etc/systemd/system/homebridge.service';
+	cmd_run 'sudo cp HB_Installation/config_GpioDevice.json /var/homebridge/config.json';
+}
+
+
+armv7_installation(){
+echo "You appear to be running on $CPU hardware."
+	cmd_run 'sudo apt-get update';
+	cmd_run 'sudo apt-get install -y git make';
+	print_status "##### Installing Node.Js  For Raspberry Pi 2/3 ######\n\n";
+	cmd_run 'curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -';
+	cmd_run 'sudo apt-get install -y nodejs'
+	sudo npm uninstall -g homebridge
+	sudo npm uninstall -g homebridge-config-ui-x
+	sudo npm uninstall -g homebridge--gpio-device
+	sudo npm uninstall -g homebridge-mqtt-switch-tasmota
+	print_status "##### Downloading Homebridge Config files ######\n\n";
+	cmd_run 'sudo rm -rf HB_Installation/'
+	cmd_run 'git clone https://github.com/Rabelbeat/HB_Installation.git';
+	cmd_run 'sudo cp HB_Installation/service_environment /etc/default/homebridge';
+	cmd_run 'sudo cp HB_Installation/rpi_2-3_Service /etc/systemd/system/homebridge.service';
+	cmd_run 'sudo cp HB_Installation/config_GpioDevice.json /var/homebridge/config.json';
+}
+ubuntu_installation(){
+echo "You appear to be running on $CPU hardware."
+	cmd_run 'sudo apt-get update';
+	cmd_run 'sudo apt-get install -y git make';
+	print_status "##### Installing Node.Js  For Ubuntu ######\n\n";
+	cmd_run 'curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -';
+	cmd_run 'sudo apt-get install -y nodejs'
+	cmd_run 'sudo npm uninstall -g homebridge';
+	cmd_run 'sudo npm uninstall -g homebridge-config-ui-x';
+	#sudo npm uninstall -g homebridge-
+	#sudo npm uninstall -g homebridge-config-ui-x
+	#sudo npm remove -g homebridge-mqtt-switch-tasmota
+	print_status "##### Downloading Homebridge Config files ######\n\n";
+	cmd_run 'git clone https://github.com/Rabelbeat/HB_Installation.git';
+	cmd_run 'sudo cp HB_Installation/service_environment /etc/default/homebridge';
+	cmd_run 'sudo cp HB_Installation/ubuntu_Service /etc/systemd/system/homebridge.service';
+	cmd_run 'sudo cp HB_Installation/configMinmal.json /var/homebridge/config.json';
+}
+
+mosquitto_installation(){
+echo "{green}Installing mosquitto & mosquitto-clients{normal}";
+cmd_run 'sudo apt-install -y mosquitto mosquitto-clients';
+
+}
+tasmota_installation(){
+echo "{green}Installing Tasmota plugin{normal}";
+		cmd_run 'sudo npm install -g --unsafe-perm homebridge-mqtt-switch-tasmota';
+		echo "Updating Homebridge config.json";
+		cmd_run 'sudo cp HB_Installation/config_Tasmota.json /var/homebridge/config.json';
+}
+shairport_installation(){
+echo "{green}Installing Shairport-Sync AirPlay server{normal}";
+  cmd_run 'sudo apt-get install -y build-essential git xmltoman autoconf automake libtool libdaemon-dev libpopt-dev libconfig-dev libasound2-dev avahi-daemon libavahi-client-dev libssl-dev libpulse-dev libsoxr-dev libmbedtls-dev';
+  cmd_run 'cd ~'
+  cmd_run 'git clone https://github.com/mikebrady/shairport-sync.git'
+  cmd_run 'cd ~/shairport-sync'
+  cmd_run 'autoreconf -fi'
+  cmd_run './configure --sysconfdir=/etc --with-alsa --with-avahi --with-ssl=openssl --with-systemd --with-metadata --with-soxr'
+  cmd_run 'sudo make'
+  cmd_run 'sudo make install'
+  cmd_run 'systemctl enable shairport-sync'
+  cmd_run 'systemctl start shairport-sync'
+}
+print_bold "
+			שלום, הסקריפט הבא יתיקן באופן אוטומטי את כל מה שנחוץ להתקנת הומברידג
+			בחר את המחשב שברשותך
+			"
+#if $(uname -m | grep -Eq ^armv6); then
+#   armv6_installation
+# elif $(uname -m | grep -Eq ^armv7); then
+#    armv7_installation
+#fi
+
+
+echo $CPU
+
+while true
 do
-    read -p "Enter your Raspberry Pi Model (a/b/z/2/3) and press [ENTER] or C to cancel:" answer
+    read -p "${bold}$Do yo want to install mosquitto MQTT broker? y/n: ${normal}" answer
     case $answer in
-        [23]* )    echo "${green}Raspberry 2/3 ARMV7 Selected${normal}";
-		exec_cmd 'sudo apt-get update';
-		exec_cmd 'sudo apt-get install -y git make';
-		print_status "##### Installing Node.Js  For Raspberry Pi 2/3 ######\n\n";
-		exec_cmd 'curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -';
-		print_status "##### Downloading Homebridge Config files ######\n\n";
-		exec_cmd 'cd /tmp';
-		exec_cmd 'git clone https://github.com/Rabelbeat/HB_Installation.git';
-		exec_cmd 'sudo cp HB_Installation/service_environment /etc/default/homebridge';
-		exec_cmd 'sudo cp HB_Installation/Rpi_2-3_Service /etc/systemd/system/homebridge.service';
-		exec_cmd 'sudo cp HB_Installation/configMinmal.json /var/homebridge/config.json';
+        [yY]* )echo "${green}Cool, Mosquitto will be install${normal}";  
+		install_mosquitto=1;
 		break;;
-		[aAbBzZ]* ) echo "{green}Raspberry A/B/Zero ARMV6 Selected${normal}";
-		exec_cmd 'sudo apt-get update';
-		exec_cmd 'sudo apt-get install -y git make';
-		print_status "##### Installing Node.Js V8.9.4 For Raspberry Pi A/B/B+/Zero ######\n\n";
-		exec_cmd 'wget https://nodejs.org/dist/v8.9.4/node-v8.9.4-linux-armv6l.tar.gz';
-		exec_cmd 'tar -xvf node-v8.9.4-linux-armv6l.tar.gz';
-		exec_cmd 'sudo cp -r node-v8.9.4-linux-armv6l/* /usr/local/';
-		print_status "##### Downloading Homebridge Config files & create systemd failover service ######\n\n";
-		exec_cmd 'cd /tmp';
-		exec_cmd 'git clone https://github.com/Rabelbeat/HB_Installation.git';
-		exec_cmd 'sudo cp HB_Installation/service_environment /etc/default/homebridge';
-		exec_cmd 'sudo cp HB_Installation/Rpi_2-3_Service /etc/systemd/system/homebridge.service';
-		exec_cmd 'sudo cp HB_Installation/configMinmal.json /var/homebridge/config.json';
+		[nN]* ) echo "${red}OK, disable Mosquitto installation${normal}";
+		install_mosquitto=0;
 		break;;
-        [cC]* ) exit;;
-         * ) echo "Please Yor Board Type.";;
+         * ) echo "Please type y/n to continue!";;
     esac
 done
-		
+
 while true 
 do
-    read -p "Do yo want to install mosquitto MQTT broker?" answer
+    read -p "${bold}Do yo want to install Tasmota plugin? y/n: ${normal}" answer
     case $answer in
-        [yY]* )  echo "{green}Installing mosquitto & mosquitto-clients{normal}";
-		exec_cmd 'sudo apt install -y mosquitto mosquitto-clients';
+        [yY]* )  echo "${green}Cool, Tasmota will be install${normal}";
+		install_tasmota=1
 		break;;
-		[nN]* ) echo "{red}Skipping mosquitto installation{normal}";
+		[nN]* ) echo "${red}OK, disable Tasmota installation${normal}";
+		install_tasmota=0;
 		break;;
-         * ) echo "Please write y/n to continue";;
+         * ) echo "Please type y/n to continue!";;
     esac
-done		
-		
-		
+done	
 
+while true 
+do
+    read -p "${bold}Do yo want to install Shairport-Sync AirPlay? y/n: ${normal}" answer
+    case $answer in
+        [yY]* )  echo "${green}Cool, Shairport-Sync will be install${normal}";
+		install_shairport=1
+		break;;
+		[nN]* ) echo "${red}OK, disable Shairport-Sync installation${normal}";
+		install_shairport=0;
+		break;;
+         * ) echo "Please type y/n to continue!";;
+    esac
+done	
+############### Starting Installation ##############
+
+post_installation
+
+#if $(uname -m | grep -Eq ^armv6); then
+#armv6_installation
+# elif $(uname -m | grep -Eq ^armv7); then
+#armv7_installation
+#elif $(uname -m | grep -Eq ^x86_64); then
+#echo "x86_64 Detect"
+#ubuntu_installation
+#fi
+
+while true
+do
+    case $CPU in
+        armv6l) armv6_installation;
+                break;;
+        armv7l) armv7_installation;
+                break;;
+        x86_64) ubuntu_installation;
+                break;;
+         * ) echo "Unknown CPU Exit installation"
+                exit;;
+    esac
+done
+
+if [ $install_mosquitto -eq 1 ]
+then
+mosquitto_installation
+else
+echo "Skipping mosquitto installation"
+fi
+
+if [ $install_shairport -eq 1 ]
+then
+shairport_installation
+else
+echo "Skipping Shairport-Sync installation"
+fi
 
 print_status "##### Usermode & Permissions Setup ######\n\n"
-exec_cmd 'sudo useradd --system homebridge'
-exec_cmd 'sudo mkdir /var/homebridge'
-exec_cmd 'sudo chmod -R 0777 /var/homebridge'
-exec_cmd 'echo "$USER    ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers'
-exec_cmd 'sudo usermod -aG gpio homebridge'
-exec_cmd 'sudo usermod -aG video pi'
-exec_cmd 'sudo usermod -aG video homebridge'
-exec_cmd 'sudo systemctl daemon-reload'
-exec_cmd 'sudo systemctl enable homebridge'
-
+#cmd_run 'sudo userdel homebridge'
+#cmd_run 'sudo rm -rf /var/homebridge'
+cmd_run 'sudo useradd --system homebridge'
+sudo mkdir /var/homebridge
+cmd_run 'sudo chmod -R 0777 /var/homebridge'
+cmd_run 'echo "homebridge    ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers'
+cmd_run 'sudo usermod -aG video $USER'
+cmd_run 'sudo usermod -aG video homebridge'
+cmd_run 'sudo systemctl daemon-reload'
+cmd_run 'sudo systemctl enable homebridge'
 
 print_status "##### Installing Homebridge Server & Config UI X, GPIO Device Plugins ######\n\n\n"
-while true 
-do
-    read -p "Do yo want to install Tasmota plugin?" answer
-    case $answer in
-        [yY]* )  echo "{green}Installing Tasmota plugin{normal}";
-		exec_cmd 'sudo npm install -g --unsafe-perm homebridge-mqtt-switch-tasmota';
-		echo "Updating Homebridge config.json";
-		exec_cmd 'sudo cp HB_Installation/config_Tasmota.json /var/homebridge/config.json';
-		break;;
-		[nN]* ) echo "{red}Skipping Tasmota installation{normal}";
-		break;;
-         * ) echo "Please write y/n to continue";;
-    esac
-done		
-exec_cmd 'sudo npm install -g --unsafe-perm homebridge'
+cmd_run 'sudo npm install -g --unsafe-perm homebridge'
+if [ $install_tasmota -eq 1 ]
+then
+tasmota_installation
+else
+echo "Skipping Tasmota installation"
+fi
+
+if $(uname -m | grep -Eq ^armv6) || $(uname -m | grep -Eq ^armv7); then
+print_status "##### Arm CPU detected > Installing GPIO Device Plugin ######\n\n\n"
+cmd_run 'sudo usermod -aG gpio homebridge'
+cmd_run 'sudo npm install -g --unsafe-perm homebridge-gpio-device'
+elif $(uname -m | grep -Eq ^x86_64); then
+print_status "##### $CPU CPU detected > Skip Installing GPIO Device Plugin ######\n\n\n"
+fi
+
 print_status "##### Installing Config UI X Plugin ######\n\n\n"
-exec_cmd 'sudo npm install -g --unsafe-perm homebridge-config-ui-x'
-print_status "##### Installing GPIO Device Plugin ######\n\n\n"
-exec_cmd 'sudo npm install -g --unsafe-perm homebridge-gpio-device'
-
-cd ~
-sudo rm install_HomebridgeAll.sh
-exec_cmd 'sudo rm -rf /tmp/HB_Installation/'
+cmd_run 'sudo npm install -g --unsafe-perm homebridge-config-ui-x'
 
 
-print_bold "Homebridge Installation Finished! Starting Homebridge\n\n
-The system will automatically reboot in 30 seconds to fix needed permisions\n\n
-Pair your iPhone/iPad after reboot !!!!"
+
+sudo rm -rf install_HomebridgeAll.sh
+cmd_run 'sudo rm -rf HB_Installation/'
+
+
+print_bold "Homebridge Installation Finished!\n
+Recommended to logout & login from system to fix needed permissions After installation\n
+Starting Homebridge..."
 sleep 5
 print_status "##### Starting Homebridge server ######\n\n"
-exec_cmd 'sudo systemctl start homebridge'
-exec_cmd 'journalctl -f -u homebridge &'
+cmd_run 'sudo systemctl start homebridge'
+cmd_run 'journalctl -f -u homebridge &'
 sleep 30
-exec_cmd 'sudo shutdown -r now'
+#cmd_run 'sudo shutdown -r now'
